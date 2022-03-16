3d203cfc2385f63d2e7996fd954c4d76965389ee
allynlin <allynlin@outlook.com>
2022-03-16 20:40:19 +0800

    2022-03-16

--- index.php
+++ index.php
@@ -1,28 +1,35 @@
 <?php
+
+//数组
+
+$array = array(1=>"龙涛",1=> "唐频", 1=>"张林", "刘洋");
+var_dump($array);
+echo "<br>";
+print_r($array);
 //$txt1 = "学习 PHP";
 //$txt2 = "RUNOOB.COM";
 //$cars = array("Volvo", "BMW", "Toyota");
-//
+
 //echo $txt1;
 //echo "<br>";
 //echo "在 $txt2 学习 PHP ";
 //echo "<br>";
 //echo "我车的品牌是 $cars[0]<hr>";
 
-function test($name, $class, $age)
-{
-    echo "我的姓名是：$name<br>";
-    echo "我的班级是：$class<br>";
-    echo "我的年龄是：$age";
-}
-
-test("龙涛", "应用2031", 19);
-
-function test1($a){
-    print "<br>hello 以下是我的自我介绍:";
-    print $a;
-}
-test1("我是龙涛");
+//function test($name, $class, $age)
+//{
+//    echo "我的姓名是：$name<br>";
+//    echo "我的班级是：$class<br>";
+//    echo "我的年龄是：$age";
+//}
+//
+//test("龙涛", "应用2031", 19);
+//
+//function test1($a){
+//    print "<br>hello 以下是我的自我介绍:";
+//    print $a;
+//}
+//test1("我是龙涛");
 
 //phpinfo();
 //$name = "runoob";
@@ -39,4 +46,3 @@ test1("我是龙涛");
 //        <p>我的第一个段落。</p>
 //EOF;
 // 结束需要独立一行且前后不能空格
-
---
+++ 判断数据类型.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <title>判断数据类型</title>
+    <link href="css/white-pink.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <meta content="light dark" name="color-scheme">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <form method="post" class="white-pink" style="margin-top: 10px">
+                <label>
+                    <span>请输入内容：</span>
+                    <input type="text" name="content">
+                </label>
+                <label style="text-align: center">
+                    <input type="submit" value="计算" class="button">
+                </label>
+            </form>
+        </div>
+        <div class="col-md-12">
+            <?php
+            //error_reporting(0);
+            //屏蔽错误
+            if ($_POST) {
+                //不要使用isset判断，$_POST数据已经存在，会返回为真
+                if ($_POST["content"]) {
+                    $content = $_POST["content"];
+                    if (isset($content)&&is_numeric($content)&&!strpos($content, '.')) {
+                        print "<br><div class='white-pink'><p class='text-center'>是整数</p></div>";
+                    }else{
+                        print "<br><div class='white-pink'><p class='text-center'>不是整数</p></div>";
+                    }
+                    print "<br><div class='white-pink'><p class='text-center'>gettype($content)</p></div>";
+                }
+            }
+            ?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
