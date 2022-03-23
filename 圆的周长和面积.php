5860f236726ad49b7cb300e1efd5593f0f132fa5
allynlin <allynlin@outlook.com>
2022-03-24 00:14:50 +0800

    2022-03-24

--- 圆的周长和面积.php
+++ 圆的周长和面积.php
@@ -7,14 +7,16 @@
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <meta content="light dark" name="color-scheme">
     <style>
-        table{
+        table {
             width: 100%;
         }
-        table,tr,td{
+
+        table, tr, td {
             outline: 1px solid black;
             text-align: center;
         }
-        tr,td{
+
+        tr, td {
             width: 50%;
         }
     </style>
@@ -90,25 +92,25 @@
                     echo "<table>";
                     echo "<tr><td colspan='2'>运算符的应用</td></tr>";
                     echo "<tr><td>面积比周长大：</td><td>";
-                    echo var_dump($mj>$zc);
+                    echo var_dump($mj > $zc);
                     echo "</td></tr>";
                     echo "<tr><td>只要面积大于200以及周长小于100就是个完美的圆，请问您做的圆完美吗：</td><td>";
-                    echo var_dump($mj>200&&$zc<100);
+                    echo var_dump($mj > 200 && $zc < 100);
                     echo "</td></tr>";
                     echo "<tr><td>只要面积大于100或者周长小于50就及格，请问您做的及格吗：</td><td>";
-                    echo $mj>100||$zc<50?"及格":"不及格";
+                    echo $mj > 100 || $zc < 50 ? "及格" : "不及格";
                     echo "</td></tr>";
                     echo "<tr><td>如果不完美，将面积自增，每加1：</td><td>";
-                    if ($mj>200&&$zc<100){
-                    echo $mj;
-                    }else{
+                    if ($mj > 200 && $zc < 100) {
+                        echo $mj;
+                    } else {
                         echo ++$mj;
                     }
                     echo "</td></tr>";
                     echo "<tr><td>如果不完美，将周长自减，每加1：</td><td>";
-                    if ($mj>200&&$zc<100){
+                    if ($mj > 200 && $zc < 100) {
                         echo $mj;
-                    }else{
+                    } else {
                         echo --$zc;
                     }
                     echo "</td></tr>";
