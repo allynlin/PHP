21b37056d68ebb27cba0f517248690acc18d170d
allynlin <allynlin@outlook.com>
2022-03-24 00:14:16 +0800

    2022-03-24

--- 圆的周长和面积.php
+++ 圆的周长和面积.php
@@ -6,6 +6,18 @@
     <link href="css/white-pink.css" rel="stylesheet" type="text/css">
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <meta content="light dark" name="color-scheme">
+    <style>
+        table{
+            width: 100%;
+        }
+        table,tr,td{
+            outline: 1px solid black;
+            text-align: center;
+        }
+        tr,td{
+            width: 50%;
+        }
+    </style>
 </head>
 <body>
 <div class="container">
@@ -22,22 +34,88 @@
             </form>
         </div>
         <div class="col-md-6">
-            <?php
-            //error_reporting(0);
-            //屏蔽错误
-            $discount_finally = "";
-            if ($_POST){
-                //不要使用isset判断，$_POST数据已经存在，会返回为真
-                if ($_POST["bj"]){
+            <div class="white-pink">
+                <?php
+                if ($_POST["bj"]) {
+                    define("pi", 3.14);
                     $bj = $_POST["bj"];
-                    $zc=2*3.14*$bj;
-                    $mj=3.14*$bj*$bj;
-                    print "<div class='white-pink'><p class='text-center'>圆的周长为：$zc<br>圆的面积为：$mj</p></div>";
-                }else{
-                    print "<div class='white-pink'><p class='text-center' style='color: #ff3838;font-weight: bold;'>请输入半径</p></div>";
+                    $zc = 2 * pi * $bj;
+                    $mj = pi * $bj * $bj;
+                    echo "<table>";
+                    echo "<tr><td colspan='2'>计算圆的周长和面积</td></tr>";
+                    echo "<tr><td>半径：</td><td>$bj</td></tr>";
+                    echo "<tr><td>周长：</td><td>$zc</td></tr>";
+                    echo "<tr><td>面积：</td><td>$mj</td></tr>";
+                    echo "</table>";
+                    echo "<hr>";
+                    echo "<table>";
+                    echo "<tr><td colspan='2'>数据类型</td></tr>";
+                    echo "<tr><td>半径：</td><td>";
+                    echo var_dump($bj);
+                    echo "</td></tr>";
+                    echo "<tr><td>周长：</td><td>";
+                    echo var_dump($zc);
+                    echo "</td></tr>";
+                    echo "<tr><td>面积：</td><td>";
+                    echo var_dump($mj);
+                    echo "</td></tr>";
+                    echo "</table>";
+                    echo "<hr>";
+                    echo "<table>";
+                    echo "<tr><td colspan='2'>检测数据类型</td></tr>";
+                    echo "<tr><td>半径是否为整形：</td><td>";
+                    echo var_dump((bool)is_int($bj));
+                    echo "</td></tr>";
+                    echo "<tr><td>周长是否由数字组成：</td><td>";
+                    echo var_dump((bool)is_numeric($zc));
+                    echo "</td></tr>";
+                    echo "<tr><td>面积是否为字符串型：</td><td>";
+                    echo var_dump((bool)is_string($mj));
+                    echo "</td></tr>";
+                    echo "</table>";
+                    echo "<hr>";
+                    echo "<table>";
+                    echo "<tr><td colspan='2'>转换数据类型</td></tr>";
+                    echo "<tr><td>将半径转换为整形：</td><td>";
+                    echo (int)$bj;
+                    echo "</td></tr>";
+                    echo "<tr><td>将周长转换为字符串型：</td><td>";
+                    echo (string)$zc;
+                    echo "</td></tr>";
+                    echo "<tr><td>将面积转换为整形：</td><td>";
+                    echo (int)$mj;
+                    echo "</td></tr>";
+                    echo "</table>";
+                    echo "<hr>";
+                    echo "<table>";
+                    echo "<tr><td colspan='2'>运算符的应用</td></tr>";
+                    echo "<tr><td>面积比周长大：</td><td>";
+                    echo var_dump($mj>$zc);
+                    echo "</td></tr>";
+                    echo "<tr><td>只要面积大于200以及周长小于100就是个完美的圆，请问您做的圆完美吗：</td><td>";
+                    echo var_dump($mj>200&&$zc<100);
+                    echo "</td></tr>";
+                    echo "<tr><td>只要面积大于100或者周长小于50就及格，请问您做的及格吗：</td><td>";
+                    echo $mj>100||$zc<50?"及格":"不及格";
+                    echo "</td></tr>";
+                    echo "<tr><td>如果不完美，将面积自增，每加1：</td><td>";
+                    if ($mj>200&&$zc<100){
+                    echo $mj;
+                    }else{
+                        echo ++$mj;
+                    }
+                    echo "</td></tr>";
+                    echo "<tr><td>如果不完美，将周长自减，每加1：</td><td>";
+                    if ($mj>200&&$zc<100){
+                        echo $mj;
+                    }else{
+                        echo --$zc;
+                    }
+                    echo "</td></tr>";
+                    echo "</table>";
                 }
-            }
-            ?>
+                ?>
+            </div>
         </div>
     </div>
 </div>
