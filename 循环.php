391395e2811c565f2d41e4a57da119746fc2ecf8
allynlin <allynlin@outlook.com>
2022-03-31 11:48:45 +0800

    2022-03-31

--- 循环.php
+++ 循环.php
@@ -25,15 +25,24 @@
             <div class="white-pink">
                 <?php
                 //有红、白、黑三种球若干个，其中红、白球共25个，白、黑球共31个，红、黑球共28个，使用for循环完成计算这三种球的个数
-                for ($red = 1; $red < 25; $red++) {//红白球共25，红黑共28，红球数量小于25
-                    for ($white = 1; $white < 25; $white++) {//红白球共25，白黑球共31，白球数量小于25
-                        for ($black = 1; $black < 28; $black++) {//白黑球共31，红黑球共28，黑球数量小于28
-                            if ($red + $white == 25 and $white + $black == 31 and $red + $black == 28) {
-                                echo "红球个数：$red<br>";
-                                echo "白球个数：$white<br>";
-                                echo "黑球个数：$black<br>";
-                            }
-                        }
+                //                for ($red = 1; $red < 25; $red++) {//红白球共25，红黑共28，红球数量小于25
+                //                    for ($white = 1; $white < 25; $white++) {//红白球共25，白黑球共31，白球数量小于25
+                //                        for ($black = 1; $black < 28; $black++) {//白黑球共31，红黑球共28，黑球数量小于28
+                //                            if ($red + $white == 25 and $white + $black == 31 and $red + $black == 28) {
+                //                                                echo "红球个数：$red<br>";
+                //                                                echo "白球个数：$white<br>";
+                //                                                echo "黑球个数：$black<br>";
+                //                            }
+                //                        }
+                //                    }
+                //                }
+                for ($white = 1; $white <= 25; $white++) {
+                    $red = 25 - $white;
+                    $black = 31 - $white;
+                    if ($red + $black == 28) {
+                        echo "红球个数：$red<br>";
+                        echo "白球个数：$white<br>";
+                        echo "黑球个数：$black<br>";
                     }
                 }
                 ?>
@@ -87,10 +96,11 @@
                 error_reporting(0);
                 if ($_POST) {
                     if ($_POST["chinese"] && $_POST["math"]) {
-                        if ($_POST["chinese"] >= 0 && $_POST["chinese"] <= 100 && $_POST["math"] && $_POST["math"] >= 0 && $_POST["math"] <= 100) {
-                            $ch = $_POST["chinese"];
-                            $ma = $_POST["math"];
-                            $cj = ($ch + $ma) / 2;
+                        if (is_numeric($_POST["chinese"]) && is_numeric($_POST["math"])) {
+                            if ($_POST["chinese"] >= 0 && $_POST["chinese"] <= 100 && $_POST["math"] && $_POST["math"] >= 0 && $_POST["math"] <= 100) {
+                                $ch = $_POST["chinese"];
+                                $ma = $_POST["math"];
+                                $cj = ($ch + $ma) / 2;
 //                            用if写
 //                            if ($cj < 60) {
 //                                echo "平均成绩是$cj<br>课后打扫卫生";
@@ -104,23 +114,27 @@
 //                                echo "平均成绩是$cj<br>放假一天";
 //                            }
 //                            用switch写
-                            switch ($cj / 10) {
-                                case 10:
-                                case 9:
-                                    echo "平均成绩是$cj<br>放假一天";
-                                    break;
-                                case 8:
-                                    echo "平均成绩是$cj<br>放假半天";
-                                    break;
-                                case 7:
-                                    echo "平均成绩是$cj<br>正常上课";
-                                    break;
-                                case 6:
-                                    echo "平均成绩是$cj<br>完成课后作业";
-                                    break;
-                                default:
-                                    echo "平均成绩是$cj<br>课后打扫卫生";
-                                    break;
+                                $c = $cj / 10;
+                                switch ((int)$c) {
+                                    case 10:
+                                    case 9:
+                                        echo "平均成绩是$cj<br>放假一天";
+                                        break;
+                                    case 8:
+                                        echo "平均成绩是$cj<br>放假半天";
+                                        break;
+                                    case 7:
+                                        echo "平均成绩是$cj<br>正常上课";
+                                        break;
+                                    case 6:
+                                        echo "平均成绩是$cj<br>完成课后作业";
+                                        break;
+                                    default:
+                                        echo "平均成绩是$cj<br>课后打扫卫生";
+                                        break;
+                                }
+                            } else {
+                                echo "成绩不符合要求，请重新输入";
                             }
                         } else {
                             echo "成绩不符合要求，请重新输入";
